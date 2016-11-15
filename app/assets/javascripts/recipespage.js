e4094315bfb13fb6d044de59dad4dcb1b83c6810
hangloos <ericloos00@gmail.com>
2016-11-15 11:13:30 -0600

    edited headers on homepage and recipes index

--- app/assets/javascripts/recipespage.js
+++ app/assets/javascripts/recipespage.js
@@ -1,28 +1,9 @@
  $(function(){
-  //attachListeners();
+  
   });
 
 
 
-// function attachListeners(){
-
-//   $("#showrecipes").on("click", function(event){
-//     showRecipes()
-//     event.preventDefault();
-//   })
-
-//   function showRecipes(){
-//   $.getJSON('/recipes', function(data){
-//     $("tbody").text("")
-//     data.forEach(function(recipe){
-//       if(recipe.user_id.toString() === user_id){
-//       var recipelink = "/recipes/" + recipe.id
-//       $("tbody").append('<li><a href="' + recipelink + '">' + recipe.name+'</a></li>')
-//       }
-//     })
-//   })
-//   }
-// }
 
 
 
